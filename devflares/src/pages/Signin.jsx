59a0c255bb75283bc86b4eb130dd019c3ae5a8c8
Anupam Nagar <anupamnagar07@gmail.com>
2022-12-23 11:56:00 +0530

    dat 1 final commit

--- devflares/src/pages/Signin.jsx
+++ devflares/src/pages/Signin.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 
 function Signin() {
   return (
-    <div class="bg-grey-lighter min-h-screen flex flex-col">
-                <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+    <div class="bg-grey-lighter min-h-screen flex flex-col ">
+                <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 ">
+                    <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full mt-8">
                         <h1 class="mb-8 text-3xl text-center">Sign In</h1>
                         
                         <input 
--- devflares/src/pages/Signup.jsx
+++ devflares/src/pages/Signup.jsx
@@ -6,7 +6,7 @@ function Signup() {
     // <!-- component -->
     <div class="bg-grey-lighter min-h-screen flex flex-col">
                 <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-                    <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full">
+                    <div class="bg-white px-6 py-8 rounded shadow-md text-black w-full mt-32">
                         <h1 class="mb-8 text-3xl text-center">Sign up</h1>
                         <input 
                             type="text"
