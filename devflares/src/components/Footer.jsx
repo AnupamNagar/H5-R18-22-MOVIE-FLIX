c8ed33017ada4e9985c800b556c874775be2f4d9
Anupam Nagar <anupamnagar07@gmail.com>
2022-12-23 11:15:40 +0530

    11:15

---
+++ devflares/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='border-t border-gray-300 h-[100px] items-center flex flex-col justify-center '>
+        <h1 className='text-white '>Made By ❤️ By Rohit & Anupam for Encarta Dev Flares 2k22</h1>
+        <p className='text-white underline cursor-pointer'> All Rights Reserved</p>
+        <p className='text-white '></p>
+      
+    </div>
+  )
+}
+
+export default Footer
--- devflares/src/pages/Home.jsx
+++ devflares/src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Main from '../components/Main'
 import Navbar from '../components/Navbar'
+import Row from '../components/Row'
+import requests from '../components/Requests'
+import Footer from '../components/Footer'
 
 function Home() {
   return (
     <div>
-      <Navbar />
+      
       <Main />
+      <Row title='Upcomig' fetchUrl={requests.requestUpcoming} />
+      <Row title='Trending' fetchUrl={requests.requestTrending} />
+      <Row title='Top Rated' fetchUrl={requests.requestTopRated} />
+      <Row title='Popular' fetchUrl={requests.requestPopular} />
+      <Row title='Horror' fetchUrl={requests.requestHorror} />
+
+      <Footer />
+
     </div>
   )
 }
